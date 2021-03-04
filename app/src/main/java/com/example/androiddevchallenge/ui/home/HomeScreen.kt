cc18858dca02d176127244a5c1615397dc192072
Muhammad Farhan <Muhammad.Farhan@dc1-arpatechonline.net>
2021-03-04 11:27:10 +0500

    Fix: Spotless Build Fail due to imprt All (*)

--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
@@ -1,7 +1,17 @@
 package com.example.androiddevchallenge.ui.home
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
