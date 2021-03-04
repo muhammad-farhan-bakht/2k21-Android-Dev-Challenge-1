82ace33995900587eb5e814ea17c758ccf5572ac
Muhammad Farhan <Muhammad.Farhan@dc1-arpatechonline.net>
2021-03-04 11:34:27 +0500

    Fix: Github Action Spotless Build Error MainActivity Not found

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.androiddevchallenge.main
+package com.example.androiddevchallenge
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
